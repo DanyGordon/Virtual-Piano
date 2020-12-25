b5dbc516c75d577ca74ae7e9159a262342af734e
DanyGordon <blablabla1715@gmail.com>
2020-12-25 02:20:57 +0200

    Initial commit
    Add source for Virtual Piano project

---
+++ script.js
@@ -0,0 +1,28 @@
+(function () {
+  window.initPiano = () => {
+    document.addEventListener('keydown', (event) => {
+      if(event.code == 'KeyA' ||
+      event.code == 'KeyW' ||
+      event.code == 'KeyS' ||
+      event.code == 'KeyE' ||
+      event.code == 'KeyD' ||
+      event.code == 'KeyF' ||
+      event.code == 'KeyT' ||
+      event.code == 'KeyG' ||
+      event.code == 'KeyY' ||
+      event.code == 'KeyH' ||
+      event.code == 'KeyU' ||
+      event.code == 'KeyG') {
+        let audio = new Audio();
+        audio.src = `./keys/${event.code.slice(3)}.mp3`;
+        audio.play()
+          .then(_ => {})
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+    })
+  }
+  window.initPiano();
+
+}());
